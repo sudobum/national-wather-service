530b9585e43c1abfa9eef9d8aa92d93bd751a19c
Gigi Kashia <gk12@priest.com>
2018-08-09 09:08:04 -0400

    change 1

---
+++ main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  8 19:35:08 2018
+
+@author: kash-py
+"""
+
+
+
+# =============================================================================
+# The National Weather Service - windchill index calculation
+# 
+# this program prints table of windchill values for wind speeds.
+# table row represents wind speed between 0 and 50 in 5-mph increments.
+# table columns represent temperatures from -20 to +60 in 10 degree increments.
+# Note: windchill index is calculated only for wind speeds greater than 3-mph.
+# =============================================================================
